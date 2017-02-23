fdcb635127e2fed82e4d8f3fcaac76b9ab8ea8d4
Jean-Francois Cere <jfcere@sherweb.com>
2017-02-22 23:15:01 -0500

    Remove unused import

--- src/app/markdown-to-html/markdown-to-html.service.ts
+++ src/app/markdown-to-html/markdown-to-html.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
 
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
