096de09ebfaf0972567a698b7da34c4a61758c18
jfcere <jfcere@sherweb.com>
2017-12-28 02:05:43 -0500

    Remove test for demo app

--- src/app/markdown-demo/markdown-demo.component.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MarkdownDemoComponent } from './markdown-demo.component';
-
-describe('MarkdownDemoComponent', () => {
-  it('should create an instance', () => {
-    const component = new MarkdownDemoComponent(null);
-    expect(component).toBeTruthy();
-  });
-});
