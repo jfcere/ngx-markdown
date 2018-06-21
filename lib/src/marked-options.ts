7735bbaa8c6dbd0ce45ff6468ed37148f9bb2fd3
jfcere <jfcere@sherweb.com>
2018-06-20 22:49:31 -0400

    Update @types/marked

--- lib/src/marked-options.ts
+++ lib/src/marked-options.ts
@@ -2,21 +2,9 @@ import { Renderer } from 'marked';
 
 export class MarkedOptions implements marked.MarkedOptions {
   /**
-   * Type: object Default: new Renderer()
-   *
-   * An object containing functions to render tokens to HTML.
+   * A prefix URL for any relative link.
    */
-  renderer?: Renderer;
-
-  /**
-   * Enable GitHub flavored markdown.
-   */
-  gfm?: boolean;
-
-  /**
-   * Enable GFM tables. This option requires the gfm option to be true.
-   */
-  tables?: boolean;
+  baseUrl?: string;
 
   /**
    * Enable GFM line breaks. This option requires the gfm option to be true.
@@ -24,29 +12,19 @@ export class MarkedOptions implements marked.MarkedOptions {
   breaks?: boolean;
 
   /**
-   * Conform to obscure parts of markdown.pl as much as possible. Don't fix any of the original markdown bugs or poor behavior.
+   * Enable GitHub flavored markdown.
    */
-  pedantic?: boolean;
+  gfm?: boolean;
 
   /**
-   * Sanitize the output. Ignore any HTML that has been input.
+   * Include an id attribute when emitting headings.
    */
-  sanitize?: boolean;
+  headerIds?: boolean;
 
   /**
-   * Mangle autolinks (<email@domain.com>).
+   * Set the prefix for header tag ids.
    */
-  mangle?: boolean;
-
-  /**
-   * Use smarter list behavior than the original markdown. May eventually be default with the old behavior moved into pedantic.
-   */
-  smartLists?: boolean;
-
-  /**
-   * Shows an HTML error message when rendering fails.
-   */
-  silent?: boolean;
+  headerPrefix?: string;
 
   /**
    * Set the prefix for code block classes.
@@ -54,14 +32,46 @@ export class MarkedOptions implements marked.MarkedOptions {
   langPrefix?: string;
 
   /**
+   * Mangle autolinks (<email@domain.com>).
+   */
+  mangle?: boolean;
+
+  /**
+   * Conform to obscure parts of markdown.pl as much as possible. Don't fix any of the original markdown bugs or poor behavior.
+   */
+  pedantic?: boolean;
+
+  /**
+   * Type: object Default: new Renderer()
+   *
+   * An object containing functions to render tokens to HTML.
+   */
+  renderer?: Renderer;
+
+  /**
+   * Sanitize the output. Ignore any HTML that has been input.
+   */
+  sanitize?: boolean;
+
+  /**
+   * Shows an HTML error message when rendering fails.
+   */
+  silent?: boolean;
+
+  /**
+   * Use smarter list behavior than the original markdown. May eventually be default with the old behavior moved into pedantic.
+   */
+  smartLists?: boolean;
+
+  /**
    * Use "smart" typograhic punctuation for things like quotes and dashes.
    */
   smartypants?: boolean;
 
   /**
-   * Set the prefix for header tag ids.
+   * Enable GFM tables. This option requires the gfm option to be true.
    */
-  headerPrefix?: string;
+  tables?: boolean;
 
   /**
    * Generate closing slash for self-closing tags (<br/> instead of <br>)
