d88efbb28dfac531e939a9611319864cf3760cab
Jean-Francois Cere <jfcere@sherweb.com>
2019-06-01 14:40:48 -0400

    Fix Ivy compilation (#160)

--- lib/src/markdown.module.spec.ts
+++ lib/src/markdown.module.spec.ts
@@ -1,62 +1,188 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 
-import { MarkdownModule } from './markdown.module';
+import { initialMarkedOptions, MarkdownModule } from './markdown.module';
 import { errorSrcWithoutHttpClient } from './markdown.service';
+import { MarkedOptions } from './marked-options';
 
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'host-comp',
   template: `
-    <div *ngIf="src; else mdData">
+    <div *ngIf="src; else dataTemplate">
       <markdown [src]="src"></markdown>
     </div>
 
-    <ng-template #mdData>
+    <ng-template #dataTemplate>
       <markdown [data]="markdown"></markdown>
     </ng-template>
   `,
 })
 class HostComponent {
-  markdown = '# Super title';
+  markdown = '# Markdown Title';
   src: string;
 }
 
-describe('MarkdownModule without HttpClient', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        CommonModule,
-        MarkdownModule.forRoot(),
-      ],
-      declarations: [HostComponent],
-    }).compileComponents();
-  }));
+describe('MarkdownModule', () => {
 
-  it('should render the markdown if not passing src attribute', () => {
+  describe('forRoot', () => {
 
-    const fixture: ComponentFixture<HostComponent> = TestBed.createComponent(
-      HostComponent,
-    );
+    it('should provide HttpClientModule when MarkdownModuleConfig.loader is provided', () => {
 
-    fixture.detectChanges();
+      TestBed.configureTestingModule({
+        imports: [
+          HttpClientModule,
+          MarkdownModule.forRoot({ loader: HttpClient }),
+        ],
+      });
 
-    const title: string = (fixture.nativeElement as HTMLElement).textContent.trim();
+      const httpClient = TestBed.get(HttpClient);
 
-    expect(title).toEqual('Super title');
+      expect(httpClient instanceof HttpClient).toBeTruthy();
+    });
+
+    it('should not provide HttpClientModule when MarkdownModuleConfig.loader is not provided', () => {
+
+      TestBed.configureTestingModule({
+        imports: [
+          MarkdownModule.forRoot(),
+        ],
+      });
+
+      const httpClient = TestBed.get(HttpClient, null);
+
+      expect(httpClient).toBeNull();
+    });
+
+    it('should not provide HttpClientModule when MarkdownModuleConfig.markedOptions is provided', () => {
+
+      TestBed.configureTestingModule({
+        imports: [
+          MarkdownModule.forRoot({
+            markedOptions: {
+              provide: MarkedOptions,
+              useValue: 'mockMarkedOptions',
+            },
+          }),
+        ],
+      });
+
+      const httpClient = TestBed.get(HttpClient, null);
+
+      expect(httpClient).toBeNull();
+    });
+
+    it('should provide marked options when provided', () => {
+
+      const mockMarkedOptions = 'mockMarkedOptions';
+
+      TestBed.configureTestingModule({
+        imports: [
+          MarkdownModule.forRoot({
+            markedOptions: {
+              provide: MarkedOptions,
+              useValue: 'mockMarkedOptions',
+            },
+          }),
+        ],
+      });
+
+      const markedOptions = TestBed.get(MarkedOptions);
+
+      expect(markedOptions).toBe(mockMarkedOptions);
+    });
+
+    it('should provide default marked options when not provided', () => {
+
+      TestBed.configureTestingModule({
+        imports: [
+          MarkdownModule.forRoot(),
+        ],
+      });
+
+      const markedOptions = TestBed.get(MarkedOptions);
+
+      expect(markedOptions).toEqual(initialMarkedOptions['useValue']);
+    });
+
+    it('should provide default marked options when loader is provided provided', () => {
+
+      TestBed.configureTestingModule({
+        imports: [
+          MarkdownModule.forRoot({ loader: HttpClient }),
+        ],
+      });
+
+      const markedOptions = TestBed.get(MarkedOptions);
+
+      expect(markedOptions).toEqual(initialMarkedOptions['useValue']);
+    });
   });
 
-  it('should throw an error when using src attribute without registering HttpClient', () => {
+  describe('forChild', () => {
 
-    const fixture: ComponentFixture<HostComponent> = TestBed.createComponent(
-      HostComponent,
-    );
+    it('should not provide any providers', () => {
 
-    fixture.componentInstance.src = '/some/path/to/file.md';
+      const forChildModule = MarkdownModule.forChild();
 
-    expect(() => fixture.detectChanges()).toThrowError(
-      errorSrcWithoutHttpClient,
-    );
+      expect(forChildModule.providers).toBeUndefined();
+    });
+
+    it('should inherit from forRoot providers', () => {
+
+      const mockMarkedOptions =  'mockMarkedOptions';
+
+      TestBed.configureTestingModule({
+        imports: [
+          HttpClientModule,
+          MarkdownModule.forRoot({
+            loader: HttpClient,
+            markedOptions: { provide: MarkedOptions, useValue: mockMarkedOptions },
+          }),
+          MarkdownModule.forChild(),
+        ],
+      });
+
+      const httpClient = TestBed.get(HttpClient);
+      const markedOptions = TestBed.get(MarkedOptions);
+
+      expect(httpClient instanceof HttpClient).toBeTruthy();
+      expect(markedOptions).toEqual(mockMarkedOptions);
+    });
+  });
+
+  describe('without HttpClient', () => {
+
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          CommonModule,
+          MarkdownModule.forRoot(),
+        ],
+        declarations: [HostComponent],
+      }).compileComponents();
+    }));
+
+    it('should render the markdown if not passing src attribute', () => {
+
+      const fixture = TestBed.createComponent(HostComponent);
+
+      fixture.detectChanges();
+
+      const title = (fixture.nativeElement as HTMLElement).textContent.trim();
+
+      expect(title).toEqual('Markdown Title');
+    });
+
+    it('should throw an error when using src attribute', () => {
+
+      const fixture = TestBed.createComponent(HostComponent);
+
+      fixture.componentInstance.src = '/some/path/to/file.md';
+
+      expect(() => fixture.detectChanges()).toThrowError(errorSrcWithoutHttpClient);
+    });
   });
 });
