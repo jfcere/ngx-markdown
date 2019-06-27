96826f6fe6e4d268ed31e99c43706335da972ed8
Jean-Francois Cere <jfcere@sherweb.com>
2019-06-27 07:33:34 -0400

    Fix build

--- lib/src/markdown.service.ts
+++ lib/src/markdown.service.ts
@@ -35,7 +35,7 @@ export class MarkdownService {
   }
 
   constructor(
-    @Inject(PLATFORM_ID) private platform: object,
+    @Inject(PLATFORM_ID) private platform: Object,
     @Optional() private http: HttpClient,
     private domSanitizer: DomSanitizer,
     options: MarkedOptions,
