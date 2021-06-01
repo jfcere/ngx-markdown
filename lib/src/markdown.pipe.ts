31d0f9a5169ad9f60b35a711da99fd3fce7e1be9
jfcere <jfcere@sherweb.com>
2021-06-01 19:30:40 -0400

    Fix stripped HTML using pipe

--- lib/src/markdown.pipe.ts
+++ lib/src/markdown.pipe.ts
@@ -1,4 +1,5 @@
 import { ElementRef, NgZone, Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { first } from 'rxjs/operators';
 
 import { MarkdownService } from './markdown.service';
@@ -9,12 +10,13 @@ import { MarkdownService } from './markdown.service';
 export class MarkdownPipe implements PipeTransform {
 
   constructor(
+    private domSanitizer: DomSanitizer,
     private elementRef: ElementRef<HTMLElement>,
     private markdownService: MarkdownService,
     private zone: NgZone,
   ) { }
 
-  transform(value: string): string {
+  transform(value: string): SafeHtml {
     if (value == null) {
       return '';
     }
@@ -30,6 +32,6 @@ export class MarkdownPipe implements PipeTransform {
       .pipe(first())
       .subscribe(() => this.markdownService.highlight(this.elementRef.nativeElement));
 
-    return markdown;
+    return this.domSanitizer.bypassSecurityTrustHtml(markdown);
   }
 }
