8885687f374b3fef9f695f6bdb61bc9899d55c64
jfcere <jfcere@sherweb.com>
2022-06-10 22:25:11 -0400

    Fix extension resolution when URL has no extension

--- lib/src/markdown.service.spec.ts
+++ lib/src/markdown.service.spec.ts
@@ -646,6 +646,19 @@ describe('MarkdowService', () => {
         http.expectOne(mockSrc).flush(mockResponse);
       });
 
+      it('should return src content without language tick when URL has no file extension', () => {
+        const mockSrc = 'https://domain.com/file/path';
+        const mockResponse = 'response-x';
+
+        markdownService
+          .getSource(mockSrc)
+          .subscribe(data => {
+            expect(data).toEqual(mockResponse);
+          });
+
+        http.expectOne(mockSrc).flush(mockResponse);
+      });
+
       it('should ignore query parameters when resolving file extension', () => {
 
         const mockSrc = './src-example/file.js?param=123&another=abc';
@@ -658,6 +671,60 @@ describe('MarkdowService', () => {
           });
 
         http.expectOne(mockSrc).flush(mockResponse);
+      });
+
+      it('should return src content correctly when using different URL pattern', () => {
+
+        const mockResponse = 'response-x';
+
+        const useCases = [
+          { url: 'https://domain.com/abc', extension: 'md' },
+          { url: 'https://domain.com/abc.js', extension: 'js' },
+          { url: 'https://domain.com/abc/def', extension: 'md' },
+          { url: 'https://domain.com/abc/def/hij.ts', extension: 'ts' },
+          { url: 'https://domain.com/abc/def/hij/jkl.tsx?mno=123', extension: 'tsx' },
+          { url: 'https://domain.com/abc?def=123&hij=456', extension: 'md' },
+
+          { url: 'http://domain.com/abc', extension: 'md' },
+          { url: 'http://domain.com/abc.js', extension: 'js' },
+          { url: 'http://domain.com/abc/def', extension: 'md' },
+          { url: 'http://domain.com/abc/def/hij.ts', extension: 'ts' },
+          { url: 'http://domain.com/abc/def/hij/jkl.tsx?mno=123', extension: 'tsx' },
+          { url: 'http://domain.com/abc?def=123&hij=456', extension: 'md' },
+
+          { url: './abc', extension: 'md' },
+          { url: './abc.js', extension: 'js' },
+          { url: './abc/def', extension: 'md' },
+          { url: './abc/def/hij.ts', extension: 'ts' },
+          { url: './abc/def/hij/jkl.tsx?mno=123', extension: 'tsx' },
+          { url: './abc?def=123&hij=456', extension: 'md' },
+
+          { url: '/abc', extension: 'md' },
+          { url: '/abc.js', extension: 'js' },
+          { url: '/abc/def', extension: 'md' },
+          { url: '/abc/def/hij.ts', extension: 'ts' },
+          { url: '/abc/def/hij/jkl.tsx?mno=123', extension: 'tsx' },
+          { url: '/abc?def=123&hij=456', extension: 'md' },
+
+          { url: 'abc/def', extension: 'md' },
+          { url: 'abc/def/hij.ts', extension: 'ts' },
+          { url: 'abc/def/hij/jkl.tsx?mno=123', extension: 'tsx' },
+          { url: 'abc?def=123&hij=456', extension: 'md' },
+        ];
+
+        useCases.forEach(({ url, extension }) => {
+          const expectedResponse = extension !== 'md'
+            ? '```' + extension + '\n' + mockResponse + '\n```'
+            : mockResponse;
+
+          markdownService
+            .getSource(url)
+            .subscribe(data => {
+              expect(data).toEqual(expectedResponse);
+            });
+
+          http.expectOne(url).flush(mockResponse);
+        });
       });
     });
 
--- lib/src/markdown.service.ts
+++ lib/src/markdown.service.ts
@@ -221,10 +221,22 @@ export class MarkdownService {
   }
 
   private handleExtension(src: string, markdown: string): string {
-    const extension = src
-      ? src.split('?')[0].split('.').splice(-1).join()
+    const urlProtocolIndex = src.lastIndexOf('://');
+    const urlWithoutProtocol = urlProtocolIndex > -1
+      ? src.substring(urlProtocolIndex + 4)
+      : src;
+
+    const lastSlashIndex = urlWithoutProtocol.lastIndexOf('/');
+    const lastUrlSegment = lastSlashIndex > -1
+      ? urlWithoutProtocol.substring(lastSlashIndex + 1).split('?')[0]
       : '';
-    return extension !== 'md'
+
+    const lastDotIndex = lastUrlSegment.lastIndexOf('.');
+    const extension = lastDotIndex > -1
+      ? lastUrlSegment.substring(lastDotIndex + 1)
+      : '';
+
+    return !!extension && extension !== 'md'
       ? '```' + extension + '\n' + markdown + '\n```'
       : markdown;
   }
