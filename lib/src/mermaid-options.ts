4130a9e45b1a81a7bc43eeca4fbbd3e8a6ffe08a
Jean-Francois Cere <jfcere@hotmail.com>
2023-11-04 15:00:14 -0400

    Update mermaid.js lib to 10.6.0

--- lib/src/mermaid-options.ts
+++ lib/src/mermaid-options.ts
@@ -1,5 +1,6 @@
 /* eslint-disable */
 export namespace MermaidAPI {
+
   export enum SecurityLevel {
     /**
      * (default) tags in text are encoded, click functionality is disabled
@@ -247,6 +248,28 @@ export namespace MermaidAPI {
     axisFormat?: string | undefined;
   }
 
+  export interface RunOptions {
+    /**
+     * The query selector to use when finding elements to render. Default: `".mermaid"`.
+     */
+    querySelector?: string;
+
+    /**
+     * The nodes to render. If this is set, `querySelector` will be ignored.
+     */
+    nodes?: ArrayLike<HTMLElement>;
+
+    /**
+     * A callback to call after each diagram is rendered.
+     */
+    postRenderCallback?: (id: string) => unknown;
+
+    /**
+     * If `true`, errors will be logged to the console, but not thrown. Default: `false`
+     */
+    suppressErrors?: boolean;
+  }
+
   export interface Config {
     /**
      * ### securityLevel
@@ -311,6 +334,8 @@ export namespace MermaidAPI {
      * The object containing configurations specific for gantt diagrams*
      */
     gantt?: GanttConfig | undefined;
+
+    /** To supress mermaid warning **/
 
     journey?: any; // [todo]
 
