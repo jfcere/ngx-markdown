1df9cdcf743bfd2c1be1f788e4cc507e16e0d2d1
CircleCI <circleci@email.com>
2024-11-20 20:45:21 +0000

    Auto-generated commit --
    Triggered by commit: https://github.com/jfcere/ngx-markdown/commit/620b3661a63336cc5a99b52d7a9356159d2d58d2
    CircleCI build: https://circleci.com/gh/jfcere/ngx-markdown/1178

--- chunk-R3OWIIC4.js
+++
@@ -1,1 +0,0 @@
-import{B as e,Db as n,K as i,N as o}from"./chunk-L73B4PLA.js";var p=class t{constructor(r){this.httpClient=r}get(r){return this.httpClient.get(r,{responseType:"text"}).pipe(e())}static{this.\u0275fac=function(a){return new(a||t)(o(n))}}static{this.\u0275prov=i({token:t,factory:t.\u0275fac,providedIn:"root"})}};export{p as a};
