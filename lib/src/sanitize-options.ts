ed9a98211adaf6657e29567f2f793d8e6c8dda59
jfcere <jfcere@hotmail.com>
2025-11-22 20:26:31 -0500

    Remove MarkdownModuleConfig.sanitize deprecated type

--- lib/src/sanitize-options.ts
+++ lib/src/sanitize-options.ts
@@ -4,6 +4,6 @@ export const SANITIZE = new InjectionToken<SecurityContext | SanitizeFunction>('
 
 export type SanitizeFunction = (html: string) => string;
 
-export function isSanitizeFunction(sanitize: SecurityContext | SanitizeFunction): sanitize is SanitizeFunction {
+export function isSanitizeFunction(sanitize: SecurityContext | SanitizeFunction | null): sanitize is SanitizeFunction {
   return typeof sanitize === 'function';
 }
